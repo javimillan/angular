92c72630a558c383e20633914ce67be750c6c4a4
javi <javimillanm@gmail.com>
2018-01-23 13:02:09 +0100

    diseño "mis ubicaciones"

---
+++ 0 ENTORNO LOCAL/myWeather_BKP230118/src/pages/about/about.ts
@@ -0,0 +1,44 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-about',
+  templateUrl: 'about.html'
+})
+export class AboutPage {
+
+  ubicacionesArray:any[];
+
+  constructor(public navCtrl: NavController) {
+
+
+    this.ubicacionesArray = [
+      {
+        name:"Cubelles",
+        temp:"20.7",
+        hour:"20",
+        tempMax:"22",
+        tempMin:"3",
+        status:"clear"
+      },
+      {
+        name:"Viladecans",
+        temp:"20.7",
+        hour:"20",
+        tempMax:"22",
+        tempMin:"3",
+        status:"cloudy"
+      },
+      {
+        name:"Calafell",
+        temp:"20.7",
+        hour:"20",
+        tempMax:"22",
+        tempMin:"3",
+        status:"rain"
+      }
+    ];
+
+  }
+
+}
