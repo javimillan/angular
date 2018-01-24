a5a7f53e6dad6b9dab3daddc50fdc763ed24a2b4
javi <javimillanm@gmail.com>
2018-01-24 13:54:04 +0100

    consulta doble segun resultados busqueda

--- 0 ENTORNO LOCAL/myWeather_BKP240118/src/pages/agregar/agregar.component.ts
+++ 0 ENTORNO LOCAL/myWeather_BKP240118/src/pages/agregar/agregar.component.ts
@@ -14,14 +14,54 @@ export class AgregarComponent implements OnInit {
   key:any = this._myWeather.key;
   dataResult:any = "";
   results:any[];
+  agregar:any = 0;
+  result:any;
+  zmw:any;
+  temp:any;
 
 
   ngOnInit() {}
 
+  selectResults(item){
+    console.log("Ciudad seleccionada: " + item.city + ", " + item.country_name);
+    console.log("Temperatura: " + item.city + ", " + item.country_name);
+    //Imprimimos valor en el input
+    this.city = item.city + ", " + item.country_name;
+    // limpiamos lista de resultados
+    this.dataResult = "";
+    this.results = [];
+    this.result = "";
+    this.agregar = 1;
+    this.zmw = item.zmw;
+    let url:any = `http://api.wunderground.com/api/${this.key}/forecast/geolookup/conditions/q/zmw:${this.zmw}.json`;
+    this._http.get(url)
+    .subscribe((resp:any) => {
+      console.log(resp);
+      // this.results = resp.response.results;
+    });
+
+  }
+  selectResult(item){
+    console.log("Ciudad seleccionada: " + this.result);
+    console.log("Temperatura: " + this.temp);
+    //Imprimimos valor en el input
+    this.city = this.result;
+    // limpiamos lista de resultados
+    this.dataResult = "";
+    this.results = [];
+    this.result = "";
+    this.agregar = 1;
+  }
   consultaData(){
     console.log("1111");
+    this.agregar = 0;
 
-    let url:any = `http://api.wunderground.com/api/${this.key}/forecast/geolookup/conditions/q/CA/${this.city}.json`;
+      let url:any = `http://api.wunderground.com/api/${this.key}/forecast/geolookup/conditions/q/${this.city}.json`;
+
+    // obtenemos URL desde ZMW en caso de existir varios resultados
+    // para evitar confusiones con varios resultados utilizamos el ZMW
+    // http://api.wunderground.com/api/e276774f8069a33e/forecast/q/zmw:31003.1.99999.json
+
     console.log("url")
     console.log(url)
     console.log(this._http.get(url))
@@ -32,18 +72,27 @@ export class AgregarComponent implements OnInit {
       // -------- mas de 1 resultado
       if (this.results != undefined){
         console.log(this.results.length);
-        this.dataResult = "-Existen varias coincidencias-";
+        this.dataResult = "- Selecciona una de las coincidencias -";
+        // this.temp = resp.current_observation.temp_c;
+        this.results.forEach(function (name) {
+          console.log(name);
+          console.log(name.city + ", " + name.country);
+          console.log("ZMW");
+          console.log(name.zmw);
+        });
       }
       else{
           // --------- 1 resultado
           if(resp.current_observation != undefined){
-            console.log(resp.current_observation.display_location.full);
-            this.dataResult = resp.current_observation.display_location.full;
+            console.log(resp.current_observation.display_location.city);
+            this.result = resp.current_observation.display_location.city;
+            this.temp = resp.current_observation.temp_c;
+            console.log(this.temp)
           }
           // -------- no hay resultados
           else{
             console.log("ERROR");
-            this.dataResult = "-No existen coincidencias-";
+            this.dataResult = "- No existen coincidencias -";
           }
       }
     });
